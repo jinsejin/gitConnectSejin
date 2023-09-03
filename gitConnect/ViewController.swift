90c6270b0a57e489163a1671f19f8d09b492701b
jinsejin <j03298969@gmail.com>
2023-09-03 20:57:27 +0900

    delete label2 that was the serve label

--- gitConnect/ViewController.swift
+++ gitConnect/ViewController.swift
@@ -14,17 +14,11 @@ class ViewController: UIViewController {
         label.text = "Pard"
         return label
     }()
-    let label2 : UILabel = {
-        let label = UILabel(frame: CGRect(x: 300, y: 100, width: 100, height: 100))
-        label.text = "Pard sejin"
-        return label
-    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         view.addSubview(label)
-        view.addSubview(label2)
     }
 }
 
