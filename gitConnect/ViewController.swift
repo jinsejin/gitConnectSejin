3d33fa93c7d4ff7717384c66753cd06d2aecfd04
jinsejin <j03298969@gmail.com>
2023-09-03 20:53:31 +0900

    add label2 that is the serve "pard sejin" label and so on

--- gitConnect/ViewController.swift
+++ gitConnect/ViewController.swift
@@ -10,14 +10,21 @@ import UIKit
 class ViewController: UIViewController {
 
     let label : UILabel = {
-        let label = UILabel()
+        let label = UILabel(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
         label.text = "Pard"
+        return label
+    }()
+    let label2 : UILabel = {
+        let label = UILabel(frame: CGRect(x: 300, y: 100, width: 100, height: 100))
+        label.text = "Pard sejin"
         return label
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
         view.addSubview(label)
+        view.addSubview(label2)
     }
 }
 
