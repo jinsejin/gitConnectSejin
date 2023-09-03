20e8c27057eac9594e5dfc73ffc1e0177eef1e70
jinsejin <j03298969@gmail.com>
2023-09-03 20:06:50 +0900

    Initial Commit iOS version change 15.5 to 16.0

---
+++ gitConnect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitConnect
+//
+//  Created by 진세진 on 2023/09/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
